71dc9e490f27d7e0823e8c5b7979edf72606374d
Ed <eyablonsky@gmail.com>
2025-03-22 20:18:53 +0200

    allow to specify count by one value

--- grm/grm.go
+++ grm/grm.go
@@ -53,6 +53,33 @@ type Count struct {
   Min int `yaml:"min"`
 }
 
+func (count Count) MarshalYAML() (interface{}, error) {
+  if count.Min == count.Max {
+    return count.Min, nil
+  }
+  return map[string]int{"min": count.Min, "max": count.Max,}, nil
+}
+
+func (count *Count) UnmarshalYAML(node *yaml.Node) error { 
+  switch node.Tag {
+  case "!!int":
+    var value int
+    if err := node.Decode(&value); err != nil {
+      return err
+    }
+    count.Min = value
+    count.Max = value
+  case "!!map":
+    var value map[string]int
+    if err := node.Decode(&value); err != nil {
+      return err
+    }
+    count.Min = value["min"]
+    count.Max = value["max"]
+  }
+  return nil
+}
+
 type RoutineExcerciseSet struct {
   Count Count                   `yaml:"count"`
   Loads []int                   `yaml:"loads,omitempty"`
